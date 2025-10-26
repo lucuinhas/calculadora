6a4e472e3f37b325efa7e11ae6dee53fa4191f27
lucuinhas <lucarvalhodealmeida@gmail.com>
2025-10-25 21:50:04 -0400

    first commit

--- src/refreshGrades.js
+++ src/refreshGrades.js
@@ -1,15 +1,42 @@
-function calculateGrades() {
+const TARGET_GRADE = 700
+
+function refreshGrades() {
+    let currFinalGrade = 0
+
     let grades = [
-        document.getElementById("firstGrade").value,
-        document.getElementById("secondGrade").value,
-        document.getElementById("thirdGrade").value,
-        document.getElementById("fourthGrade").value,
+        document.getElementById("firstGrade"),
+        document.getElementById("secondGrade"),
+        document.getElementById("thirdGrade"),
+        document.getElementById("fourthGrade"),
     ]
 
-    for(let currGrade of grades) {
-        if(parseInt(currGrade) > 100) currGrade = 100;
-        if(parseInt(currGrade) < 0) currGrade = 0;
+    let currBimesters = []
+
+    for(let i of grades.keys()) {
+        let currGrade = grades[i];
+
+        if(currGrade.value.trim() !== '') {
+            currGrade.value = parseInt(currGrade.value);
+
+            if(parseInt(currGrade.value) > 100) currGrade.value = 100;
+            if(parseInt(currGrade.value) < 0) currGrade.value = 0;
+
+            currFinalGrade += parseInt(currGrade.value) * (i + 1);
+        }
+        else {
+            currBimesters.push(i + 1)
+        }
     }
 
-    console.log("Fuck you mama mia")
+    let currBimVal = 0
+
+    for(let i of currBimesters) {
+        currBimVal += i
+    }
+
+    for(let currGrade of grades) {
+        console.log(currBimesters)
+        console.log(currFinalGrade)
+        currGrade.placeholder = Math.ceil((TARGET_GRADE - currFinalGrade) / currBimVal)
+    }
 }
